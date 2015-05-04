b86ec7acd82c85e28d5e8ffc9422e8432f346c50
Simon Xie <simon.xie@kuulabu.com>
2015-05-04 23:58:36 +0800

    added example for searching records with regex (select ... where like)

---
+++ node-js/mongo-read-search/mongo-read-search.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test3'); //no need to create db in advance
+
+var TodoSchema = new mongoose.Schema({ //no need to create table in advance
+  name: String,
+  completed: Boolean,
+  note: String,
+  updated_at: { type: Date, default: Date.now },
+});
+var Todo = mongoose.model('Todo', TodoSchema);
+
+
+
+
+var http = require('http');
+var express = require('express');
+var base64 = require('base64-js');
+
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+
+app.get('/todo/search/:keyword/', function (req, res) {
+
+	console.log("keyword=" + req.params.keyword);
+
+	var regex = new RegExp(req.params.keyword, "i");
+
+	Todo.find({"name": regex}, function (err, todos) {
+		if (err) return console.error(err);
+		console.log(todos);
+
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		res.end(JSON.stringify({
+			"isSuccess": true,
+			"foundCount": todos.length,
+			"todos": todos,
+		}));
+	});
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+
